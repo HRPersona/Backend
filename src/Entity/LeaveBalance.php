2d7d4f2dac4a2445d207374188aa17d4f456e8b1
ad3n <surya.kejawen@gmail.com>
2017-07-13 10:48:03 +0700

    update

---
+++ src/Entity/LeaveBalance.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Persona\Hris\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model\SoftDeletable\SoftDeletable;
+use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
+use Persona\Hris\Core\Logger\ActionLoggerAwareInterface;
+use Persona\Hris\Core\Logger\ActionLoggerAwareTrait;
+use Persona\Hris\Employee\Model\EmployeeInterface;
+use Persona\Hris\Leave\Model\EmployeeLeaveBalanceInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="a_leave_balances")
+ *
+ * @ApiResource(
+ *     attributes={
+ *         "filters"={"order.filter"},
+ *         "normalization_context"={"groups"={"read"}},
+ *         "denormalization_context"={"groups"={"write"}}
+ *     }
+ * )
+ *
+ * @author Muhamad Surya Iksanudin <surya.iksanudin@personahris.com>
+ */
+class LeaveBalance implements EmployeeLeaveBalanceInterface, ActionLoggerAwareInterface
+{
+    use ActionLoggerAwareTrait;
+    use Timestampable;
+    use SoftDeletable;
+
+    /**
+     * @Groups({"read"})
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="guid")
+     *
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @Groups({"read", "write"})
+     * @ORM\ManyToOne(targetEntity="Persona\Hris\Entity\Employee", fetch="EAGER")
+     * @ORM\JoinColumn(name="employee_id", referencedColumnName="id")
+     * @Assert\NotBlank()
+     *
+     * @var EmployeeInterface
+     */
+    private $employee;
+
+    /**
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="integer")
+     * @Assert\NotBlank()
+     *
+     * @var int
+     */
+    private $leaveDay;
+
+    /**
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="integer")
+     * @Assert\NotBlank()
+     *
+     * @var int
+     */
+    private $leaveBalance;
+
+    /**
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     *
+     * @var string
+     */
+    private $remark;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return EmployeeInterface
+     */
+    public function getEmployee(): ? EmployeeInterface
+    {
+        return $this->employee;
+    }
+
+    /**
+     * @param EmployeeInterface $employee
+     */
+    public function setEmployee(EmployeeInterface $employee = null): void
+    {
+        $this->employee = $employee;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLeaveDay(): int
+    {
+        return $this->leaveDay;
+    }
+
+    /**
+     * @param int $leaveDay
+     */
+    public function setLeaveDay(int $leaveDay): void
+    {
+        $this->leaveDay = $leaveDay;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLeaveBalance(): int
+    {
+        return $this->leaveBalance;
+    }
+
+    /**
+     * @param int $leaveBalance
+     */
+    public function setLeaveBalance(int $leaveBalance): void
+    {
+        $this->leaveBalance = $leaveBalance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRemark(): string
+    {
+        return $this->remark;
+    }
+
+    /**
+     * @param string $remark
+     */
+    public function setRemark(string $remark): void
+    {
+        $this->remark = $remark;
+    }
+}
